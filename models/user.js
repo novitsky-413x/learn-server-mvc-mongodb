62445b039d1c161272d2ad6a833b83bccb8f5ac0
Alexander Novitsky <novitsky.413x@gmail.com>
2025-03-06 18:44:24 +0300

    finished controller logic for adding/deleting cart items

--- models/user.js
+++ models/user.js
@@ -33,6 +33,35 @@ class User {
         return db.collection('users').updateOne({ _id: this._id }, { $set: { cart: updatedCart } });
     }
 
+    getCart() {
+        const db = getDb();
+        const productIds = this.cart.items.map((i) => i.productId);
+        return db
+            .collection('products')
+            .find({ _id: { $in: productIds } })
+            .toArray()
+            .then((products) => {
+                return products.map((p) => {
+                    return {
+                        ...p,
+                        quantity: this.cart.items.find((i) => i.productId.toString() === p._id.toString())
+                            .quantity,
+                    };
+                });
+            })
+            .catch((err) => console.log(err));
+    }
+
+    deleteItemFromCart(productId) {
+        const updatedCartItems = this.cart.items.filter(
+            (item) => item.productId.toString() !== productId.toString()
+        );
+        const db = getDb();
+        return db
+            .collection('users')
+            .updateOne({ _id: this._id }, { $set: { cart: { items: updatedCartItems } } });
+    }
+
     static findById(userId) {
         const db = getDb();
         return db
